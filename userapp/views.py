b9f40b2426cf490c93f395e03ce20be26dfed9c5
sachin <{ID}+{username}@users.noreply.github.com>
2024-11-19 19:57:27 +0100

    add pagination

--- userapp/views.py
+++ userapp/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-from .models import UserApp
-from rest_framework import viewsets
-from .models import UserApp
-from .serializers import UserAppSerializer
-
-from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 import django_filters
 from rest_framework import viewsets
 from rest_framework.decorators import action
-
-from datetime import datetime, timedelta
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+from django.utils import timezone
+from datetime import datetime, timedelta
 import requests
 
-from django.utils import timezone
-from datetime import timedelta
-from rest_framework.pagination import LimitOffsetPagination
+from .models import UserApp
+from .serializers import UserAppSerializer
 
+from rest_framework.pagination import PageNumberPagination
+
+class UserAppPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class UserAppViewSet(viewsets.ModelViewSet):
     queryset = UserApp.objects.all()
     serializer_class = UserAppSerializer
-
+    pagination_class = UserAppPagination
