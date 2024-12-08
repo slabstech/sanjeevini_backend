24562feeda4fd310bd520979104045abfc0755a0
sachin <26170834+sachinsshetty@users.noreply.github.com>
2024-12-08 23:53:33 +0100

    code-cleanup

--- inference/tool_call.py
+++ inference/tool_call.py
@@ -1,30 +1,25 @@
 import prance
 from typing import List
-from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
-from mistral_common.protocol.instruct.request import ChatCompletionRequest
-from mistral_common.protocol.instruct.tool_calls import Function, Tool
 from mistral_common.protocol.instruct.messages import UserMessage
 import json
 import requests
 import functools
-import os
-from ollama import Client
 import ollama
 
 
-def getPetById(petId: int) -> str:
+def getDoctorById(id: int) -> str:
     try:
         method = 'GET'
         headers=None
         data=None
-        url =  'http://localhost:8000/api/v1/doctorapp/' + str(petId)
+        url =  'http://localhost:8000/api/v1/doctorapp/' + str(id)
         response = requests.request(method, url, headers=headers, data=data)
         # Raise an exception if the response was unsuccessful
         response.raise_for_status()
         #response = make_api_call('GET', url + str(petId))
         if response.ok :
             json_response = response.json()
-            if petId == json_response['id']:
+            if id == json_response['id']:
                 return json_response
         return json.dumps({'error': 'Pet id not found.'})
     except requests.exceptions.HTTPError as e:
@@ -51,12 +46,15 @@ def getUserByName(username: str) -> str:
             return json.dumps({'error': 'Error with API.'})
 
 names_to_functions = {
-  'getPetById': functools.partial(getPetById, petId=''),
-  'getUserByName': functools.partial(getUserByName, username='')  
+    'get_doctorapp': functools.partial(getDoctorById, id='')
 }
 
+#names_to_functions = {
+#  'getDoctorById': functools.partial(getDoctorById, petId=''),
+#  'getUserByName': functools.partial(getUserByName, username='')  
+#}
 
-import requests
+
 
 def load_model(ollama_url, model_name):
     command = "/api/pull"
@@ -86,10 +84,8 @@ def get_user_messages(queries: List[str]) -> List[UserMessage]:
     return user_messages
 
 def generate_tools_prompt(objs, function_end_point):
-    params = ['operationId', 'description',  'parameters']
     parser = prance.ResolvingParser(function_end_point, backend='openapi-spec-validator')
     spec = parser.specification
-    #print(spec)
 
     system_message = """
         You are a helpful assistant that parses OpenAPI specifications. Here are the key instructions:
@@ -127,23 +123,19 @@ def generate_tools_prompt(objs, function_end_point):
         }
     }
     """
-
-    # Combine the parts
     user_message = user_message_part1 + user_message_part2 
 
-# Combine and send the messages
 
-    # Combine and send the messages
     combined_prompt = f"{system_message}\n\n{user_message}"
     
     model='mistral'
     result = execute_prompts(prompt=combined_prompt, model=model)
     #print(result['response'])
     function_value = result['response']
-    print(function_value)
+    #print(function_value)
 
     tool_json_value = json.loads(function_value) 
-    print(tool_json_value)
+    #print(tool_json_value)
     return tool_json_value
     
 
@@ -173,49 +165,37 @@ def execute_prompts(prompt, model="llama3.2",url ="http://localhost:11434/api/ge
 def execute_generator():
     #queries = ["What's the status of my Pet 1?", "Find information of user user1?" ,  "What's the status of my Store Order 3?"]
     #return_objs = [['pet','petId'], ['user', 'username'], ['store/order','orderId']]
-    queries = ["What's the status of my Pet 1?"]
+    queries = ["What is the status of User 1?"]
     return_objs = [['doctorapp','id']]
     function_end_point =  'http://localhost:8000/api/schema/?format=json'
     user_messages=get_user_messages(queries)
-    #user_tools = generate_tools(return_objs, function_end_point)
-    user_tools = generate_tools_prompt(return_objs, function_end_point)
+    #TODO - fix generate_tool_prompts to return correct json
+    #user_tools = generate_tools_prompt(return_objs, function_end_point)
 
-    #create tokens for message and tools prompt
-    tokenizer = MistralTokenizer.v3()
-    completion_request = ChatCompletionRequest(tools=user_tools, messages=user_messages,)
-    tokenized = tokenizer.encode_chat_completion(completion_request)
-    _, text = tokenized.tokens, tokenized.text
-
-    model = "mistral:7b"
-    prompt = text 
-    #print(prompt)
-    #if ollama_endpoint_env is None:
-    #    ollama_endpoint_env = 'http://localhost:11434'
-    url = "http://localhost:11434/api/generate"
-    headers = {
-        "Content-Type": "application/json"
+    user_tools_str = """
+    {
+    "type": "function",
+    "function": {
+        "name": "get_doctorapp",
+        "description": "Get a doctor appointment by id",
+        "parameters": {
+        "type": "object",
+        "properties": {
+            "id": {
+            "type": "integer"
+            }
+        },
+        "required": ["id"]
+        }
     }
-    payload = {
-        "model": "mistral",
-        "prompt": prompt,
-        "stream": False,
-        "raw": True,
-
     }
+    """
+    user_tools_json = json.loads(user_tools_str)
+    #print(user_tools_json)
+    tool_calls = tool_prompts_executor(user_tools_json)
 
-    result = ''
-    try:
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        result = response.json()
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"Other error occurred: {err}")
+    process_tool_calls(tool_calls, names_to_functions)
 
-    print(result)
-    #result = response.json
-    process_results(result, user_messages)
 
 
 class Function:
@@ -227,77 +207,21 @@ class ToolCall:
     def __init__(self, function):
         self.function = function
  
-def process_results(result, messages=None):
 
-    #result_format = result['response'].split("\n\n")
-    #result_tool_calls = result_format[0].replace("[TOOL_CALLS] ","")
-
-    #tool_calls = json.loads(result)
-    tool_calls = result
-    
-    index = 0 
+def process_tool_calls(tool_calls, names_to_functions):
+    index = 0
     try:
         for tool_call in tool_calls:
-            function_name = tool_call["name"]
-            function_params = (tool_call["arguments"]) 
-            #print(messages[index].content)
+            function_name = tool_call.function.name
+            function_params = tool_call.function.arguments
             function_result = names_to_functions[function_name](**function_params)
             print(function_result)
-            index = index + 1
-    except:
-        print(function_name + " is not defined")
-
-#execute_generator()
-
-def get_objects_parameters(url):
-    parser = prance.ResolvingParser(url, backend='openapi-spec-validator')
-    spec = parser.specification
-    object_names = spec['components']['schemas'].keys()
-    for key in list(object_names):
-        #print(key)
-        properties = spec['components']['schemas'][key]
-        #print(properties)
+            index += 1
+    except KeyError:
+        print(f"{function_name} is not defined")
 
 
-#tool_call_function()
-#execute_generator()
-#return_objs = [['doctorapp','id']]
-#function_end_point =  'http://localhost:8000/api/schema/?format=json'
-#user_tools = generate_tools_prompt(return_objs, function_end_point)
-#print(user_tools)
-
-def ollama_tools():
-    response = ollama.chat(
-        model='mistral',
-        messages=[{'role': 'user', 'content':
-            'What is the weather in Toronto?'}],
-
-            # provide a weather checking tool to the model
-        tools=[{
-        'type': 'function',
-        'function': {
-            'name': 'get_current_weather',
-            'description': 'Get the current weather for a city',
-            'parameters': {
-            'type': 'object',
-            'properties': {
-                'city': {
-                'type': 'string',
-                'description': 'The name of the city',
-                },
-            },
-            'required': ['city'],
-            },
-        },
-        },
-    ],
-    )
-
-    print(response['message']['tool_calls'])
-
-
-
-def ollama_tools_prompt(tools_json):
+def tool_prompts_executor(tools_json):
     response = ollama.chat(
         model='mistral',
         messages=[{'role': 'user', 'content':
@@ -307,51 +231,8 @@ def ollama_tools_prompt(tools_json):
         tools=[tools_json],
     )
 
-    print(response['message']['tool_calls'])
+    #print(response['message']['tool_calls'])
     tool_calls_result = response['message']['tool_calls']
     return tool_calls_result
 
-user_tools_str = """
-{
-  "type": "function",
-  "function": {
-    "name": "get_doctorapp",
-    "description": "Get a doctor appointment by id",
-    "parameters": {
-      "type": "object",
-      "properties": {
-        "id": {
-          "type": "integer"
-        }
-      },
-      "required": ["id"]
-    }
-  }
-}
-"""
-user_tools_json = json.loads(user_tools_str)
-#print(user_tools_json)
-tool_calls = ollama_tools_prompt(user_tools_json)
-#process_results(tool_call_results)
-
-def process_tool_calls(tool_calls, names_to_functions):
-    index = 0
-    try:
-        for tool_call in tool_calls:
-            function_name = tool_call.function.name
-            function_params = tool_call.function.arguments
-            # Print message content if needed
-            # print(messages[index].content)
-            function_result = names_to_functions[function_name](**function_params)
-            print(function_result)
-            index += 1
-    except KeyError:
-        print(f"{function_name} is not defined")
-
-# Example usage
-#tool_calls = [ToolCall(function=Function(name='get_doctorapp', arguments={'id': 1}))]
-names_to_functions = {
-    'get_doctorapp': lambda id: f"Doctor appointment details for ID {id}"
-}
-
-process_tool_calls(tool_calls, names_to_functions)
+execute_generator()
